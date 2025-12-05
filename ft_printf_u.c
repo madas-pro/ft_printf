1495e61d605b7f9cc9a4cac63585d77a4611101f
Adam Olivier <adolivie@f1r8s21.clusters.42paris.fr>
2025-12-05 11:12:51 +0100

    pxXm left

--- ft_printf_u.c
+++ ft_printf_u.c
@@ -6,7 +6,7 @@
 /*   By: adolivie <adolivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 01:02:18 by adolivie          #+#    #+#             */
-/*   Updated: 2025/12/05 10:53:09 by adolivie         ###   ########.fr       */
+/*   Updated: 2025/12/05 11:12:25 by adolivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_printf_u(unsigned int n)
 	count = 0;
 	if (n >= 10)
 	{
-		ft_printf_u(n / 10);
+		count += ft_printf_u(n / 10);
 	}
 	ft_putchar_fd(n % 10 + '0', 1);
 	count += 1;
